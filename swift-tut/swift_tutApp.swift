e58c7c37f0b3dc7a4e2b49bc9906fc43e0f9831d
codewithdarkwa <jnrdrkw1@gmail.com>
2023-09-16 21:28:49 +0000

    Initial Commit

---
+++ swift-tut/swift_tutApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_tutApp.swift
+//  swift-tut
+//
+//  Created by Darkwa John on 16/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct swift_tutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
